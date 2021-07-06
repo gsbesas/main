9d09aa4c57529b4aa313854f7882fe277692bb66
Waseem <wassim.sheikh@gmail.com>
2021-07-07 01:38:15 +0530

    exp date new funtion and computed field added

--- gsbesas_product_label/models/product_template.py
+++ gsbesas_product_label/models/product_template.py
@@ -1,9 +1,20 @@
 from odoo import api, fields, models, tools, _
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
-    exp_date = fields.Date(string='Exp Date', copy=False)
+    exp_date = fields.Datetime('Exp Date', compute='get_last_po_date')
     contents = fields.Char('Contents')
     manufacture = fields.Char('Manufacturer')
+    
+    def get_last_po_date(self):
+        for record in self:
+            move_line = self.env['stock.move.line'].search(
+                [('location_id.usage', '=', 'supplier'),('location_dest_id.usage', '=', 'internal')],
+                order='date desc',
+                limit=1
+            )
+            if move_line:
+                record['exp_date'] = move_line.expiration_date
